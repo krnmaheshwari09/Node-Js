ef5c9f75ca8599535ade6a3cc3a2a720ba1739db
Karan Maheshwari <krnmaheshwari2003@gmail.com>
2024-04-10 23:23:11 +0530

    Methods done

--- 02-Server/index.js
+++ 02-Server/index.js
@@ -1,14 +1,32 @@
 import * as http from "http"
 import * as fs from "fs"
+import * as url from "url"
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Request Recieved\n`
+    if(req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Recieved\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
-        switch(req.url){
-            case '/': res.end("HomePage");
-            break;
-            case '/about': res.end("I am Karan Maheshwari");
-            break;
+        switch(myUrl.pathname){
+            case '/':
+                if(req.method === 'GET') res.end("HomePage");
+                break;
+            case '/about':
+                const username = myUrl.query.user;
+                res.end(`Hi ${username}`);
+                break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end("Here are your results for " + search);
+                break;
+            case '/signup':
+                if(req.method === 'GET'){
+                    res.end("This is a signup Form");
+                }else if(req.method === 'POST'){
+                    res.end("Success");
+                }
+                break;
             default: res.end("Error 404: Not Found");
         }
     })
