a0ceb37fcf2a79a85f772b4af4fc9bc7db72bae0
Karan Maheshwari <krnmaheshwari2003@gmail.com>
2024-04-12 00:01:59 +0530

    Project 01 is done. Complete program with uses of all http methods(GET/Post/...)

---
+++ Project-01/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const fs = require("fs");
+const users = require("./data.json");
+
+
+const app = express();
+const PORT = 3000;
+
+// Middlewares - almost like plugin in express
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html);
+})
+
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+
+app
+    .route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        const id = Number(req.params.id);
+        const body = req.body;
+        const index = users.findIndex(user => user.id === id);
+        if(index !== -1){
+            users[index] = {...users[index], ...body};
+            fs.writeFile('./data.json', JSON.stringify(users), (err, data) => {
+                return res.json({ status: "updated", id: id });
+            });
+        }else{
+            return res.json({ status: "Id not found"});
+        }
+    })
+    .delete((req, res) => {
+        const id = Number(req.params.id);
+        const index = users.findIndex(user => user.id === id);
+        if(index !== -1){
+            users.splice(index, 1);
+            fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
+                return res.json({ status: "deleted" });
+            });
+        }else{
+            return res.json({ status: "Id not found"});
+        }
+    })
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    // users.push({
+    //     first_name: body.first_name,
+    //     last_name: body.last_name,
+    //     email: body.email,
+    //     gender: body.gender,
+    //     job_title: body.job_title
+    // });
+    users.push({id: users.length + 1,...body});
+    fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "success", id: users.length});
+    })
+});
+
+// app.patch('/api/users/:id', (req, res) => {
+//     // TODO: Edit the user with id
+//     return res.json({ status: "pending"});
+// });
+
+// app.delete('/api/users/:id', (req, res) => {
+//     // TODO: Delete the user with id
+//     return res.json({ status: "pending"});
+// });
+
+app.listen(PORT, () => console.log(`Server started at Port: http://localhost:${PORT}/`));
