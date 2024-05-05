93e4aa93929263faf53c379c0747ef0cd1a8bbbf
Karan Maheshwari <krnmaheshwari2003@gmail.com>
2024-05-06 00:10:43 +0530

    some ui is created

--- Short_URL/index.js
+++ Short_URL/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import path from "path";
 import urlRoute from "./routes/url.js";
 import connectMongoDB from "./config.js";
+import staticRouter from './routes/staticRouter.js'
+import URL from './models/url.js';
 
 const app = express();
 const PORT = 3000;
@@ -9,9 +12,16 @@ connectMongoDB("mongodb+srv://krnmaheshwari09:Karan2004@test-data.edzavq1.mongod
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("Error Occur", err));
 
+// Setting Embedded js for rendering html file
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
 app.use("/url", urlRoute);
+app.use("/", staticRouter);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
